cfb0cba6e064dffbb0b0b0ae8ddcb99b32558549
kprzepiora89@o2.pl <Sekwana1#>
2021-01-12 19:09:43 +0100

    first commit

---
+++ src/main/java/com/premerleagueapp/premerleagueapp/PremerleagueappApplication.java
@@ -0,0 +1,13 @@
+package com.premerleagueapp.premerleagueapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremerleagueappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PremerleagueappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/premerleagueapp/premerleagueapp/PremerleagueappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.premerleagueapp.premerleagueapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremerleagueappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
